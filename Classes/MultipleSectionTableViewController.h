fb69f05fcba13200794c0aa48a20410fda227dda
Florian Reiss <85802@supinfo.com>
2013-01-22 14:47:06 -0800

    Adding TableView
    Adding classes for table view.
    Will manage all the content.
    Use of 2 classes instead of 1 (easier).
    Multiple section -> No detail ; SingleSection -> Detail.

---
+++ Classes/MultipleSectionTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  MultipleSectionTableViewController.h
+//  myResume
+//
+//  Created by Florian Reiss on 22/01/13.
+//  Copyright (c) 2013 Florian Reiss. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MultipleSectionTableViewController : UITableViewController
+
+@end
---
+++ DetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  DetailViewController.h
+//  myResume
+//
+//  Created by Florian Reiss on 22/01/13.
+//  Copyright (c) 2013 Florian Reiss. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UITableViewController
+
+@end
