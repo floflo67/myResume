78a4a34adb9fb04b80105aa873bbca928ae3e4eb
Florian Reiss <85802@supinfo.com>
2013-01-30 19:41:21 -0800

    Adding XMLParser file
    Creation of the file that will countain the Parser.

---
+++ Parser/XMLParser.h
@@ -0,0 +1,13 @@
+//
+//  XMLParser.h
+//  myResume
+//
+//  Created by Florian Reiss on 30/01/13.
+//  Copyright (c) 2013 Florian Reiss. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface XMLParser : NSXMLParser
+
+@end
---
+++ Parser/XMLParser.m
@@ -0,0 +1,13 @@
+//
+//  XMLParser.m
+//  myResume
+//
+//  Created by Florian Reiss on 30/01/13.
+//  Copyright (c) 2013 Florian Reiss. All rights reserved.
+//
+
+#import "XMLParser.h"
+
+@implementation XMLParser
+
+@end
