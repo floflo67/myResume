730d2eb800a9192f5d980bf68ae1d6b25f15404f
Florian Reiss <85802@supinfo.com>
2013-02-13 18:35:33 -0800

    Adding SkillsViewController

---
+++ Classes/SkillsViewController.h
@@ -0,0 +1,13 @@
+//
+//  SkillsViewController.h
+//  myResume
+//
+//  Created by Florian Reiss on 13/02/13.
+//  Copyright (c) 2013 Florian Reiss. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SkillsViewController : UIViewController
+
+@end
---
+++ Classes/SkillsViewController.m
@@ -0,0 +1,38 @@
+//
+//  SkillsViewController.m
+//  myResume
+//
+//  Created by Florian Reiss on 13/02/13.
+//  Copyright (c) 2013 Florian Reiss. All rights reserved.
+//
+
+#import "SkillsViewController.h"
+
+@interface SkillsViewController ()
+
+@end
+
+@implementation SkillsViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
