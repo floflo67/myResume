3fb3037974f4ab960b2f80ccd4f284f8a489377c
Florian Reiss <85802@supinfo.com>
2013-02-13 18:36:01 -0800

    Adding JobsViewController

---
+++ Classes/JobsViewController.h
@@ -0,0 +1,13 @@
+//
+//  JobsViewController.h
+//  myResume
+//
+//  Created by Florian Reiss on 13/02/13.
+//  Copyright (c) 2013 Florian Reiss. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JobsViewController : UIViewController
+
+@end
