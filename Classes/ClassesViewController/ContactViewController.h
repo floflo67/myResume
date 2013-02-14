7e3aeaf55184138e2f91c031cc3c459f11ab2194
Florian Reiss <85802@supinfo.com>
2013-02-13 18:41:24 -0800

    Adding ContactViewController

---
+++ Classes/ClassesViewController/ContactViewController.h
@@ -0,0 +1,13 @@
+//
+//  ContactViewController.h
+//  myResume
+//
+//  Created by Florian Reiss on 13/02/13.
+//  Copyright (c) 2013 Florian Reiss. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ContactViewController : UIViewController
+
+@end
