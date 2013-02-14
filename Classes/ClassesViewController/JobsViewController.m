d9da830327d397536bf41f5dd71fc0d38b9232e6
Florian Reiss <85802@supinfo.com>
2013-02-13 18:41:58 -0800

    Adding JobsViewController

---
+++ Classes/ClassesViewController/JobsViewController.m
@@ -0,0 +1,38 @@
+//
+//  JobsViewController.m
+//  myResume
+//
+//  Created by Florian Reiss on 13/02/13.
+//  Copyright (c) 2013 Florian Reiss. All rights reserved.
+//
+
+#import "JobsViewController.h"
+
+@interface JobsViewController ()
+
+@end
+
+@implementation JobsViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
