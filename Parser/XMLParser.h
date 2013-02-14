78143a9bc6c39a5ea6afc04d00822b39b5ecc700
Florian Reiss <85802@supinfo.com>
2013-02-13 18:20:07 -0800

    Update XMLParser file
    Changing file to implement functions from .h.

--- Parser/XMLParser.h
+++ Parser/XMLParser.h
@@ -8,6 +8,26 @@
 
 #import <Foundation/Foundation.h>
 
-@interface XMLParser : NSXMLParser
+@interface XMLParser : NSXMLParser <NSXMLParserDelegate>
+
++(id)initWithContentsOfURL:(NSURL *)url;
++(id)initWithData:(NSData *)data;
++(BOOL)parse;
+
++(void)parserDidStartDocument:(NSXMLParser *)parser;
+
++(void)parserDidEndDocument:(NSXMLParser *)parser;
+
++(void)parser:(NSXMLParser *)parser
+                    didStartElement:(NSString *)elementName
+                    namespaceURI:(NSString *)namespaceURI
+                    qualifiedName:(NSString *)qName
+                    attributes:(NSDictionary *)attributeDict;
+
++(void)parser:(NSXMLParser *)parser
+                    didEndElement:(NSString *)elementName
+                    namespaceURI:(NSString *)namespaceURI
+                    qualifiedName:(NSString *)qName;
+
 
 @end
--- Parser/XMLParser.m
+++ Parser/XMLParser.m
@@ -10,4 +10,37 @@
 
 @implementation XMLParser
 
++(id)initWithContentsOfURL:(NSURL *)url {
+    return NULL;
+}
+
++(id)initWithData:(NSData *)data {
+    return NULL;
+}
+
++(BOOL)parse {
+    return YES;
+}
+
++(void)parserDidStartDocument:(NSXMLParser *)parser {
+    
+}
+
++(void)parserDidEndDocument:(NSXMLParser *)parser {
+    
+}
+
++(void)parser:(NSXMLParser *)parser didStartElement:(NSString *)elementName
+                                    namespaceURI:(NSString *)namespaceURI
+                                    qualifiedName:(NSString *)qName
+                                    attributes:(NSDictionary *)attributeDict {
+    
+}
+
++(void)parser:(NSXMLParser *)parser didEndElement:(NSString *)elementName
+                                    namespaceURI:(NSString *)namespaceURI
+                                    qualifiedName:(NSString *)qName {
+    
+}
+
 @end
